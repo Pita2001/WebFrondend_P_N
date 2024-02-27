c5cef21ab8425af463d48d3aec08db8856cfc9fa
Pita2001 <szarazpeter2001@gmail.com>
2024-02-27 13:32:27 +0100

    sorrend

--- src/sajatosztalyok/AdminProba.js
+++ src/sajatosztalyok/AdminProba.js
@@ -1,18 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Button, FlatList, Text, View } from 'react-native';
+import { ActivityIndicator, Button, Text, View } from 'react-native';
 
-const IP=require('./Ipcim')
+const IP = require('./Ipcim');
 
 const AdminProba = () => {
-  const [csere, setAscending] = useState(true);
+  const [csere, setCsere] = useState({ evjarat: true, modell: true, hengerurtartalom: true });
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const getMovies = async () => {
+  const getAutok = async () => {
     try {
-      const response = await fetch(IP.Ipcim+'autok');
-      const json = await response.json();
-      setData(json);
+      const response = await fetch(IP.Ipcim + 'autok');
+      const autok = await response.json();
+      setData(autok);
     } catch (error) {
       console.error(error);
     } finally {
@@ -20,37 +20,57 @@ const AdminProba = () => {
     }
   };
 
-  const EvSzures = () => {
+  const rendezes = (oszlop) => {
     const sortedData = [...data].sort((a, b) => {
-      if (csere) {
-        return a.auto_evjarat - b.auto_evjarat;
-      } else {
-        return b.auto_evjarat - a.auto_evjarat;
+      if (oszlop === 'evjarat') {
+        return csere.evjarat ? a.auto_evjarat - b.auto_evjarat : b.auto_evjarat - a.auto_evjarat;
+      } else if (oszlop === 'modell') {
+        return csere.modell ? a.auto_modell.localeCompare(b.auto_modell) : b.auto_modell.localeCompare(a.auto_modell);
+      } else if (oszlop === 'hengerurtartalom') {
+        return csere.hengerurtartalom ? a.auto_hengerurt - b.auto_hengerurt : b.auto_hengerurt - a.auto_hengerurt;
       }
     });
     setData(sortedData);
-    setAscending(!csere);
+    setCsere(prevState => ({ ...prevState, [oszlop]: !prevState[oszlop] }));
   };
 
   useEffect(() => {
-    getMovies();
+    getAutok();
   }, []);
 
   return (
-    <View style={{ flex: 1, padding: 24 }}>
-      <Button title={csere ? 'Évjárat szerint növekvő' : 'Évjárat szerint csökkenő'} onPress={EvSzures} />
+    <View style={{ flex: 1, padding: 24}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' , marginBottom: 10, paddingLeft: '95px'}}>
+      <Button title={`Modell ${csere.modell ? 'növekvő' : 'csökkenő'}`} onPress={() => rendezes('modell')} color="#ff5733" />
+      <Button title={`Évjárat ${csere.evjarat ? 'növekvő' : 'csökkenő'}`} onPress={() => rendezes('evjarat')} color="#ff5733" />
+        <Button title={`Hengerűrtartalom ${csere.hengerurtartalom ? 'növekvő' : 'csökkenő'}`} onPress={() => rendezes('hengerurtartalom')} color="#ff5733" />
+      </View>
       {isLoading ? (
-        <ActivityIndicator />
+        <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({ id }) => id}
-          renderItem={({ item }) => (
-            <Text>
-              {item.auto_modell}, {item.auto_evjarat}
-            </Text>
-          )}
-        />
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 1, borderRightWidth: 1 }}>
+            <Text style={{ fontWeight: 'bold', textAlign: 'center', borderBottomWidth: 1 }}>Modell</Text>
+            {!isLoading && data.map(item => (
+              <Text key={item.id} style={{ textAlign: 'center', borderBottomWidth: 1 }}>{item.auto_modell}</Text>
+            ))}
+          </View>
+
+          <View style={{ flex: 1, borderRightWidth: 1 }}>
+            <Text style={{ fontWeight: 'bold', textAlign: 'center', borderBottomWidth: 1 }}>Évjárat</Text>
+            {!isLoading && data.map(item => (
+              <Text key={item.id} style={{ textAlign: 'center', borderBottomWidth: 1 }}>{item.auto_evjarat}</Text>
+            ))}
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text style={{ fontWeight: 'bold', textAlign: 'center', borderBottomWidth: 1 }}>Hengerürtartalom</Text>
+            {!isLoading && data.map(item => (
+              <Text key={item.id} style={{ textAlign: 'center', borderBottomWidth: 1 }}>{item.auto_hengerurt}</Text>
+            ))}
+          </View>
+
+        </View>
       )}
     </View>
   );
--- src/sajatosztalyok/ProbaAdmin_2.js
+++ src/sajatosztalyok/ProbaAdmin_2.js
@@ -1,15 +1,18 @@
-import React, {useEffect, useState} from 'react';
-import {ActivityIndicator, FlatList, Text, View} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { ActivityIndicator, Button, Text, View } from 'react-native';
+
+const IP = require('./Ipcim');
 
 const ProbaAdmin_2 = () => {
+  const [csere, setCsere] = useState({ evjarat: true, modell: true, hengerurtartalom: true });
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const getMovies = async () => {
+  const getMotorok = async () => {
     try {
-      const response = await fetch('https://reactnative.dev/movies.json');
-      const json = await response.json();
-      setData(json.movies);
+      const response = await fetch(IP.Ipcim + 'motorok');
+      const Motorok = await response.json();
+      setData(Motorok);
     } catch (error) {
       console.error(error);
     } finally {
@@ -17,24 +20,57 @@ const ProbaAdmin_2 = () => {
     }
   };
 
+  const rendezes = (oszlop) => {
+    const sortedData = [...data].sort((a, b) => {
+      if (oszlop === 'evjarat') {
+        return csere.evjarat ? a.motor_evjarat - b.motor_evjarat : b.motor_evjarat - a.motor_evjarat;
+      } else if (oszlop === 'modell') {
+        return csere.modell ? a.motor_modell.localeCompare(b.motor) : b.motor_modell.localeCompare(a.motor_modell);
+      } else if (oszlop === 'hengerurtartalom') {
+        return csere.hengerurtartalom ? a.motor_hengerurt - b.motor_hengerurt : b.motor_hengerurt - a.motor_hengerurt;
+      }
+    });
+    setData(sortedData);
+    setCsere(prevState => ({ ...prevState, [oszlop]: !prevState[oszlop] }));
+  };
+
   useEffect(() => {
-    getMovies();
+    getMotorok();
   }, []);
 
   return (
-    <View style={{flex: 1, padding: 24}}>
+    <View style={{ flex: 1, padding: 24}}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between' , marginBottom: 10, paddingLeft: '95px'}}>
+      <Button title={`Modell ${csere.modell ? 'növekvő' : 'csökkenő'}`} onPress={() => rendezes('modell')} color="#ff5733" />
+      <Button title={`Évjárat ${csere.evjarat ? 'növekvő' : 'csökkenő'}`} onPress={() => rendezes('evjarat')} color="#ff5733" />
+        <Button title={`Hengerűrtartalom ${csere.hengerurtartalom ? 'növekvő' : 'csökkenő'}`} onPress={() => rendezes('hengerurtartalom')} color="#ff5733" />
+      </View>
       {isLoading ? (
-        <ActivityIndicator />
+        <ActivityIndicator size="large" color="#0000ff" />
       ) : (
-        <FlatList
-          data={data}
-          keyExtractor={({id}) => id}
-          renderItem={({item}) => (
-            <Text>
-              {item.title}, {item.releaseYear}
-            </Text>
-          )}
-        />
+        <View style={{ flexDirection: 'row' }}>
+          <View style={{ flex: 1, borderRightWidth: 1 }}>
+            <Text style={{ fontWeight: 'bold', textAlign: 'center', borderBottomWidth: 1 }}>Modell</Text>
+            {!isLoading && data.map(item => (
+              <Text key={item.id} style={{ textAlign: 'center', borderBottomWidth: 1 }}>{item.motor_modell}</Text>
+            ))}
+          </View>
+
+          <View style={{ flex: 1, borderRightWidth: 1 }}>
+            <Text style={{ fontWeight: 'bold', textAlign: 'center', borderBottomWidth: 1 }}>Évjárat</Text>
+            {!isLoading && data.map(item => (
+              <Text key={item.id} style={{ textAlign: 'center', borderBottomWidth: 1 }}>{item.motor_evjarat}</Text>
+            ))}
+          </View>
+
+          <View style={{ flex: 1 }}>
+            <Text style={{ fontWeight: 'bold', textAlign: 'center', borderBottomWidth: 1 }}>Hengerürtartalom</Text>
+            {!isLoading && data.map(item => (
+              <Text key={item.id} style={{ textAlign: 'center', borderBottomWidth: 1 }}>{item.motor_hengerurt}</Text>
+            ))}
+          </View>
+
+        </View>
       )}
     </View>
   );
